1a9d5348e1a59b599d728e17bffeba83f6f12535
Malaga-Karl <kbkmalaga2021@plm.edu.ph>
2024-03-06 15:40:04 +0800

    added formatted excel. plus instructions in autotest.py

--- autotest.py
+++ autotest.py
@@ -2,18 +2,16 @@ import syntax
 import csv
 import pandas as pd
 import lexer
+import openpyxl
+from openpyxl.styles import PatternFill
 
-
-
+# exception si 252
+# if want niyo try, download https://docs.google.com/spreadsheets/d/1xzn_b8FHk7-cKC7gAeeUlDnoTKlbY8DXVyR3XfJoj6o/edit#gid=1770890317 as csv
+# then rename it to aeyetest.csv and put it in A-EYE folder (bali marereplace niya ung current)
+# run this and check results.xlsx for results
 df = pd.read_csv('A-EYE\\aeyetest.csv')
-# for index, row in df.iterrows():
-#     result = syntax.analyze_syntax(row['test_item'])
-#     sh.update(f"b{index}", result)
-
-# print(syntax.analyze_syntax(df[0]))
 
 def analyze_syntax(code):
-    # res = [i for j in code.split() for i in (j, ' ')][:-1]
     result, errors = lexer.analyze_text(code)
     if not errors:
         syntax_result = syntax.analyze_syntax(result)
@@ -26,8 +24,9 @@ def analyze_syntax(code):
 
 
 string = df['test_item'][0]
-with open('file.csv', 'w', newline='') as file:
+with open('A-EYE\\file.csv', 'w', newline='') as file:
     writer = csv.writer(file)
+    writer.writerow(['Test Item', 'Expected', 'Detail', 'Remarks', 'Result'])
     for index, row in df.iterrows():
         string = row['test_item']
         expected = row['expected']
@@ -42,3 +41,24 @@ with open('file.csv', 'w', newline='') as file:
         
         remarks = 'Pass' if detail == expected else 'Fail'
         writer.writerow([string, str(expected).strip(), detail, remarks, result ])
+
+file = pd.read_csv('A-EYE\\file.csv', encoding='unicode_escape')
+results = pd.ExcelWriter('A-EYE\\results.xlsx')
+file.to_excel(results, index=False)
+results.save()
+wb = openpyxl.load_workbook('A-EYE\\results.xlsx')
+ws = wb['Sheet1']
+
+passed = PatternFill(patternType='solid', fgColor='32a862')
+fail = PatternFill(patternType='solid', fgColor='ff0000')
+for i in range(2, len(df) + 2):
+    if ws[F'D{i}'].value == 'Pass':
+        ws[F'D{i}'].fill = passed
+    else:
+        ws[F'D{i}'].fill = fail
+
+wb.save('A-EYE\\results.xlsx')
+
+number_of_passed = file.value_counts('Remarks')['Pass']
+print(f'Passing Rate: {round(number_of_passed/len(file)*100,2)}%')
+
