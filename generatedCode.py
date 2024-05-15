3fcc18b35f243981966ba96162b84fd54c59a5d6
Malaga-Karl <kbkmalaga2021@plm.edu.ph>
2024-05-15 23:42:59 +0800

    hanggang while loop

--- generatedCode.py
+++ generatedCode.py
@@ -1,12 +1,6 @@
 def main():
-    a = 10; 
-    match(a):
-        case 1: print("the case is 1"); 
-        case 2: print("the case is 2");  
-        case 3: print("the case is 3");  
-        case 4: print("the case is 4");  
-        case 5: print("the case is 5");  
-        case _: print("no number"); 
+    for a in range(10, 0, 1):
+        print(a); 
     
 
 
