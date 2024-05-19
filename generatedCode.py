02b6e9626c66fc18a837551f9e08b2c7cb7995ac
Elahnor <rhicasison2021@plm.edu.ph>
2024-05-20 00:59:44 +0800

    Connected RunBtn

--- generatedCode.py
+++ generatedCode.py
@@ -1,11 +1,13 @@
 def main():
-    a = 10; 
-    b = 12; 
-    c = 13; 
-    d = a + b + c; 
-    if(d == 35):
-        print("Working"); 
-    
+	n = 3; 
+	match(n):
+		case 1: print("you selected 1!"); 
+		case 2: print("2 is my favorite number!"); 
+		case 3: print("3 is the magic number!"); 
+		case 4: print("I don't like 4"); 
+		case 5: print("last number"); 
+		case _: print("i dont think you followed instructions"); 
+	
 
 
 if __name__ == '__main__':
--- generator.py
+++ generator.py
@@ -31,19 +31,22 @@ statement_replacements = {
     'pint ': '',
     'fleet ': '',
     'doffy ': '',
-    'bull ': ''
+    'bull ': '',
+    '++': '+=1',
+    '--': '-=1',
+    'helm': 'match',
+    'chest': 'case',
+    'dagger': 'case _',
+    'void'  : 'def',
+    'loyal ': ''
 }
 
 def nth_repl(s, sub, repl, n):
     find = s.find(sub)
-    # If find is not -1 we have found at least one match for the substring
     i = find != -1
-    # loop util we find the nth or we find no match
     while find != -1 and i != n:
-        # find + 1 means we start searching from after the last match
         find = s.find(sub, find + 1)
         i += 1
-    # If i is equal to n we found nth match so replace
     if i == n:
         return s[:find] + repl + s[find+len(sub):]
     return s
@@ -61,20 +64,25 @@ def remove_dtye(line):
 
 def generate(code):
     # Split code by lines
-    code = code.split("\n")
+    code = [line.strip() for line in code.split("\n")]
     pyfile = open("generatedCode.py", "w")
     firstLine = code.index("onboard") + 1
     lastLine = code.index("offboard")
-
+    activeBrackets = 0
     inside_comment = False
+    inside_ForLoop = False
+    for_iteration = 0
+
+    variables = {}
     
     # Temporary variable counter: will start the count in t1 
     # temp_var_counter = 1
     # temp_vars = {}
-
     # Iterate through lines
     for i in range(firstLine, lastLine):
+        hadOBracket = False
         line = code[i]
+        
         if line == "":
             continue
         
@@ -94,98 +102,153 @@ def generate(code):
         if '#' in code[i]:
             code[i] = code[i].split('#')[0]  # Remove the comment part
             code[i] = code[i].rstrip()  # Remove trailing whitespace
+            
+
+        # Check for brackets
+        if '{' in line:
+            hadOBracket = True
+        if '}' in line:
+            if inside_ForLoop:
+                activeBrackets -=2
+                inside_ForLoop = False
+            else:
+                activeBrackets -= 1
+        
 
         firstWord = line.split()[0] if len(line.split()) > 1 else ""
         
-        for key in statement_replacements.keys():
-            code[i] = code[i].replace(key, statement_replacements[key])
-            
+
+
         if firstWord in ['pint', 'fleet', 'doffy', 'bull', 'void']:
-            firstWord = firstWord + ' '
-            if "()" in line.split()[1]:  # Function statement    
-                line = line.replace(firstWord, 'def ')
-                parameters = re.findall(r'\(.*?\)', line)
-                if len(parameters) > 0:
-                    if "fleet" in parameters: line = line.replace("fleet ", "")
-                    if "doffy" in parameters: line = line.replace("doffy ", "")
-                    if "bull" in parameters: line = line.replace("bull ", "")
+            secondWord = line.split()[1]
+            if '(' in secondWord:
+                words = line.split()
+                words[0] = 'def'
+                line = ' '.join(words)
             else:
-                line = line.replace(firstWord, '')
-            # else:  # Variable statement
-            #     length = len(line.split())
-            #     afterType = " ".join(line.split()[1:])        
-            #     line = line.replace(firstWord, afterType)
-            #     # line=replacenth(line, afterType, "", 2)
-            #     line = nth_repl(line, afterType, "", 2)
-        
-        if 'four(' in line:
-            in_for = re.findall(r'\(.*?\)', line)
-            in_for_no_parenthesis = str(in_for[0].replace('(', '').replace(')', ''))
-            in_for_no_parenthesis = str("".join(in_for_no_parenthesis))
-            in_for_no_parenthesis = remove_dtye(in_for_no_parenthesis)
-            in_for_split = in_for_no_parenthesis.split(';')
+                thirdWord = line.split()[2]
+                if '(' in thirdWord:
+                    words = line.split()
+                    words[0] = 'def'
+                    line = ' '.join(words)
 
-           
-            variable = in_for_split[0].split()[0]
-            var_value = in_for_split[0].split()[2]
-            ending_point = in_for_split[1].split()[2]
-            condition = in_for_split[1].replace(variable, var_value)
-            if eval(condition):
-                    starting_point = var_value
-                    if ending_point.isnumeric():
-                        ending_point = ending_point if '=' not in condition else str(int(ending_point) + 1)
-                    else:
-                        ending_point = ending_point if '=' not in condition else str(ending_point + "+1")
-            else:
-                starting_point = ending_point
+        if ',' in line:
+            isParam = False
+            isArray = False
+            for letter in line:
+                if letter == '[':
+                    isArray = True
+                if letter == ']':
+                    isArray = False
+                if letter == '(':
+                    isParam = True
+                if letter == ')':
+                    isParam = False
+                if letter == ',' and not isParam and not isArray:
+                    line = line.replace(letter, "; ", 1)
+            
+   
+
+                
+
+        if 'four' in line:
+            activeParenthesis = 0
+            words_inside_for_loop = ''
+            four_index = line.index('four')
+            four_subset = line[four_index+4:]
+            four_subset = four_subset.rstrip('{')
+            print("four_subset: ", four_subset)
+
+            for char in four_subset:
+                if char == '(' and activeParenthesis == 0:
+                    activeParenthesis += 1
+                elif char == '(' and activeParenthesis > 0:
+                    activeParenthesis += 1
+                    words_inside_for_loop += char
+                elif char == ')' and activeParenthesis > 1:
+                    activeParenthesis -= 1
+                    words_inside_for_loop += char
+                elif char == ')' and activeParenthesis == 1:
+                    activeParenthesis -= 1
+                else:
+                    words_inside_for_loop += char
+            
+            print("words inside for: ", words_inside_for_loop)
+            words_with_parenthesis = '(' + words_inside_for_loop + ')'
+
+            for_iteration = 0
+
+            in_for_split = [item.strip() for item in words_inside_for_loop.split(';')]
+
+            for_decl = in_for_split[0].strip()
+            for_cond = in_for_split[1].strip()
+            for_update = in_for_split[2].strip()
+
+
+            starting_point = for_decl.split('=')[1].strip()
+            variable = remove_dtye(for_decl.split('=')[0]).strip()
+            print("starting point: ", starting_point)
+            print("variable: ", variable)
+
+            if '=' in for_cond:
+                ending_point = for_cond.split('=')[1].strip()
+            elif '<' in for_cond:
+                ending_point = for_cond.split('<')[1].strip()
+            elif '>' in for_cond:
+                ending_point = for_cond.split('>')[1].strip()
+            
+            print(for_cond)
+            print("ending point: ", ending_point)
+            condition = in_for_split[1].replace(variable,starting_point)
+
+            print("condition", condition)
+
+            update = in_for_split[2]
+            if '++' in update:
+                step = 1
+            if '--' in update:
+                step = -1
             
 
+            line = f'theo({condition})' + '{\n' + ('\t' * (activeBrackets + 1)) + line + '}'
+            if ending_point.isnumeric():
+                ending_point = ending_point if '=' not in condition else str(int(ending_point) + 1)
+            else:
+                ending_point = ending_point if '=' not in condition else str(ending_point + "+1")
+        
+            
+            line = line.replace(words_with_parenthesis, f' {variable} in range({starting_point}, {ending_point}, {step})')
+            inside_ForLoop = True
+            
 
-
-            # # if '++' in in_for[2]:
-            # #     step = 1
-            # # elif '--' in in_for[2]:
-            # #     step = -1
-            step = 1
-            line = line.replace(in_for[0], f' {variable} in range({starting_point}, {ending_point}, {step})')
         for key in statement_replacements.keys():
-            line = line.replace(key, statement_replacements[key])
+            if "\"" in line:
+                first_quote = line.find("\"") + 1
+                last_quote = line.rfind("\"")
+                prequote_substring = line[:first_quote]
+                postquote_substring = line[last_quote:]
+                line = prequote_substring.replace(key, statement_replacements[key]) + line[first_quote:last_quote] + postquote_substring.replace(key, statement_replacements[key])
+            else:
+                line = line.replace(key, statement_replacements[key])
+
+        if '=' in line:
+            decl = [item.strip() for item in line.split(";") if item != '']
+
+            for item in decl:
+                if '=' in item:
+                    declare = item.split("=")
+                    variables[declare[0].strip()] = declare[1].strip()
+            
+        pyfile.write(('\t'*activeBrackets) + line +'\n')
+        print(variables)
+            
+        if inside_ForLoop:
+            if for_iteration == 0:
+                activeBrackets += 1
+            for_iteration += 1
+        if hadOBracket:
+            activeBrackets += 1
         
-        # for( i = 5; i <= 5; i++)
-        # i = 5
-        # i <= 5
-        # i ++
-        #  i = 5
-            
-        # Application of TAC in Expressions
-        # if 'fire' in line: #Detection of fire
-        #     expression = line.split('fire ')[1] #If there is Fire, extraction of expression will happen
-        #     terms = expression.split() #Tokenization
-        #     result_var = terms[0] #First Term in expression represents the variable
-        #     new_expression = '' #Starting Point of Constant Replacement
-        #     for term in terms[1:]:
-        #         if term in replacements:
-        #             new_expression += replacements[term] + ' '
-        #         else:
-        #             new_expression += term + ' '
-        #     temp_var = f't{temp_var_counter}' #Hold intermiate result
-        #     temp_vars[temp_var] = new_expression #Replacemnrs
-        #     line = f'{temp_var} = {new_expression}\n' #Assignment of value
-        #     code.insert(i + 1, f'fire {result_var} {temp_var}\n')
-        #     temp_var_counter += 1 #Increment
-            
-        # Example: 
-        # fire (a + b * c / a) will be converted to:
-        # t1 = c / a
-        # t2 = b * t1
-        # t3 = a + t2
-        # fire t3
-
-        pyfile.write(line + '\n')
-
-    # # Out the Temporary Used Variables 
-    # for var, expression in temp_vars.items():
-    #     pyfile.write(f'{var} = {expression}\n')
 
     pyfile.write("\nif __name__ == '__main__':\n    main()")
     pyfile.close()
