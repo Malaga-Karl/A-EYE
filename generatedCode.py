8dc56a442978c2b7e22403d20a6db5871f6e5101
Elahnor <rhicasison2021@plm.edu.ph>
2024-05-20 01:08:36 +0800

    Fixed '

--- generatedCode.py
+++ generatedCode.py
@@ -1,12 +1,10 @@
-def main():
-	n = 3; 
-	match(n):
-		case 1: print("you selected 1!"); 
-		case 2: print("2 is my favorite number!"); 
-		case 3: print("3 is the magic number!"); 
-		case 4: print("I don't like 4"); 
-		case 5: print("last number"); 
-		case _: print("i dont think you followed instructions"); 
+def main() :
+	full = 100; 
+	current = 50; 
+	if(current/100 < 0.15):
+		print("refuel!"); 
+	else:
+		print("no need"); 
 	
 
 
