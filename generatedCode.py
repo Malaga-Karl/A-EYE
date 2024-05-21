b83904fa57fbf01b947cd5e616a632e2fd5cb6a9
Elahnor <rhicasison2021@plm.edu.ph>
2024-05-21 12:11:20 +0800

    Adding Load

--- generatedCode.py
+++ generatedCode.py
@@ -3,8 +3,9 @@ from custom_popup_input import show_custom_popup
 def main():
 	
 
-	a = "Hale"; 
-	print("Name: " + a); 
+	a = 5; 
+	a = int(show_custom_popup("[ Pint ] " + "new a: ")); 
+	print (a + 1); 
 
 
 if __name__ == '__main__':
