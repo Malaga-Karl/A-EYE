820f5b29e7ed436d21bb5833eaca91cb7b95880d
Elahnor <rhicasison2021@plm.edu.ph>
2024-05-26 15:21:03 +0800

    Fixed Runtime

--- generatedCode.py
+++ generatedCode.py
@@ -3,23 +3,23 @@ from custom_popup_input import show_custom_popup
 def main():
 	
 
-	user_input=show_custom_popup("[ PINT ] " + "Enter Number of Steps:")
-	row = (lambda x: int(x) if x.lstrip("-").isdigit() else (print("[ Error ] Invalid input. Type Mismatch") or exit()))(user_input); 
-	i = 1
-	while i <= row:
-		j = 1
-		while j <= row:
-			if (j <= i):
-				print("*", end=""); 
-			
-			else :
-				print(" ", end=""); 
-			
-			j+=1
+	factorial =int( 1); 
+	user_input=show_custom_popup("[ PINT ] " + "Enter a positive integer: ")
+	number = (lambda x: int(x) if x.lstrip("-").isdigit() else (print("[ Error ] Invalid input. Type Mismatch") or exit()))(user_input); 
+	if (number < 0):
+		print("Error: Factorial is not defined for negative numbers.\n", end=""); 
+	
+	else :
+		i = 1
+		while i <= number:
+			factorial =int( factorial * i); 
+			i = i + 1
 
-		print("\n", end=""); 
-		i+=1
-
+	
+	print("Factorial of ", end=""); 
+	print(number, end=""); 
+	print(" is ", end=""); 
+	print(factorial, end=""); 
 
 
 if __name__ == '__main__':
